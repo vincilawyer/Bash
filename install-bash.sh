d851c0ad488578908c555fedab66572f5fe456d8
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-04-09 22:40:41 +0800

    Update install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+    echo "当前版本号为$current_Vesion"
 if [[ -z $current_Vesion ]]; then
     echo "正在下载Vultr-Debian11脚本"
     wget --no-cache https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh -O /usr/local/bin/vinci
@@ -7,6 +7,6 @@ if [[ -z $current_Vesion ]]; then
     vinci
 else
     Vesion=$(curl -s https://example.com/myscript.sh | grep 'Version=' | cut -d'=' -f2)
-    echo "The version is: $Vesion"
+    echo "最新版本号为: $Vesion"
 fi
     
