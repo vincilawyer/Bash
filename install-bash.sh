60507a0d98eebe4d6da6a8e4ce403670ccaf37ae
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-04-10 00:22:46 +0800

    Update install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -9,15 +9,20 @@ else
        wget --no-cache https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh -O /usr/local/bin/vinci
        chmod +x /usr/local/bin/vinci
        echo "强制更新已完成！"
-    fi
-    Version=$(curl -s https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh | grep 'Version=' | cut -d'=' -f2)
-    echo "当前版本号$current_Version，最新版本号为$Version"
-    if [ "${current_Version}" == "${Version}" ]; then
-       echo "当前已是最新版本，无需更新！当前版本为V$Version"
     else
-       wget --no-cache https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh -O /usr/local/bin/vinci
-       chmod +x /usr/local/bin/vinci
-       echo "已更新至V$Version版本！"
+        Version=$(curl -s https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh | grep 'Version=' | cut -d'=' -f2)
+        #临时代码
+        current_Version=$(echo "${current_Version}" | tr -d '[A-Za-z]')
+        Version=$(echo "${Version}" | tr -d '[A-Za-z]')
+        
+        echo "当前版本号$current_Version，最新版本号为$Version"
+        if [ "${current_Version}" == "${Version}" ]; then
+           echo "当前已是最新版本，无需更新！当前版本为V$Version"
+        else
+           wget --no-cache https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh -O /usr/local/bin/vinci
+           chmod +x /usr/local/bin/vinci
+           echo "已更新至V$Version版本！"
+        fi
     fi
     sleep 8
 fi
