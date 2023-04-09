2f1227febf3a3c1351bbe3a7ce4ca23a5612c4f5
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-04-10 00:50:44 +0800

    Update install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -11,8 +11,8 @@ else
        echo "强制更新已完成！"
     else
         Version=$(curl -s https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh | grep 'Version=' | cut -d'=' -f2 | head -1)
-        echo "当前版本号$current_Version，最新版本号为$Version"        
-        if [[ "${current_Version}" == "{str2}" ]]; then
+        echo "当前版本号$current_Version，最新版本号为$Version"    
+        if [[ "${current_Version}" == "${Version}" ]]; then
            echo "当前已是最新版本，无需更新！当前版本为V$Version"
         else
            wget --no-cache https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh -O /usr/local/bin/vinci
