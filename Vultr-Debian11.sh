72420eb15cfb989dcc1c8f1a003889562a46df71
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-04-09 13:57:06 +0800

    Update Vultr-Debian11.sh

--- Vultr-Debian11.sh
+++ Vultr-Debian11.sh
@@ -61,11 +61,13 @@ function change_login_password {
 
     # 修改账户密码
   if [[ -n $ssh_password ]]; then
-     if echo "root:$ssh_password" | chpasswd 2>&1 | grep -q "password unchanged"; then
+    chpasswd_output=$(echo "root:$ssh_password" | chpasswd 2>&1)
+    if echo "$chpasswd_output" | grep -q "password unchanged"; then
        echo -e "${GREEN}SSH登录密码修改失败${NC}"
-     else
+    else
        echo -e "${GREEN}SSH登录密码已修改${NC}"
-     fi
+       echo "$chpasswd_output" >&2
+    fi
   fi
 }
 
