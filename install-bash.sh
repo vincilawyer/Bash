f1e0f505c18968844795f4788cebde9fe75c9fe5
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-04-09 23:53:20 +0800

    Update install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -5,7 +5,7 @@ if [[ -z $current_Version ]]; then
     chmod +x /usr/local/bin/vinci
     vinci
 else
-    if [ "$current_Version" == force ]; then
+    if [ "$current_Version" == "force" ]; then
        wget --no-cache https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh -O /usr/local/bin/vinci
        chmod +x /usr/local/bin/vinci
        echo "强制更新已完成！"
