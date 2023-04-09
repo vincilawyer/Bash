82ff8f252ea5352d9f4c82d207173a9960f333bf
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-04-09 23:02:53 +0800

    Update install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -3,7 +3,6 @@ if [[ -z $current_Version ]]; then
     echo "正在下载Vultr-Debian11脚本"
     wget --no-cache https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh -O /usr/local/bin/vinci
     chmod +x /usr/local/bin/vinci
-    vinci
 else
     echo "当前版本号为$current_Version"
     Version=$(curl -s https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh | grep 'Version=' | cut -d'=' -f2)
