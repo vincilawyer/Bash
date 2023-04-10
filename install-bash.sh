a1474a0baeea0770ea7ff91a88cdfda8ac8bf5f8
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-04-10 17:37:57 +0800

    Update install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -1,81 +1,78 @@
 #!/bin/bash
 
-function
-
-if [[ -z $current_Version ]]; then
-    echo "正在下载Vultr-Debian11脚本"
+function download {
     wget --no-cache https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh -O /usr/local/bin/vinci
     chmod +x /usr/local/bin/vinci
-    Version=$(cat /usr/local/bin/vinci | grep 'Version=' | cut -d'=' -f2 | head -1)
+    Version=$(cat /usr/local/bin/vinci | grep 'Version=' | head -1 | cut -d'=' -f2)
     if [[ -z $Version ]]; then
       echo "下载失败，请检查网络！"
-    else
-      echo "管理系统V$Version已下载完成，即将进入系统！"
       sleep 3
-
+      exit 0
+    else
+      echo "$1$Version$2"
+      execute
     fi
+}
+
+function execute {
+    sleep 3
+    clear
+    vinci
+    #如新版本存在错误，则重新更新
+    if  [ ! $? == 0 ]; then
+       while true; do
+          echo "新版本存在错误，正在尝试重新更新！...输入任意键退出"
+          #每隔59秒更新一次
+          read -t 59 -n 1 input
+          # 如果用户输入不为空，则退出更新
+          if [ ! -z $input ]; then
+              break
+          fi  
+          #强制更新
+          wget --no-cache https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh -O /usr/local/bin/vinci
+          chmod +x /usr/local/bin/vinci
+          clear
+          vinci
+          #如新版本没有错误，则执行新版本
+          if  [ $? == 0 ]; then
+             break
+          fi
+       done
+    fi
+    exit 1
+}
+
+function main {
+
+#下载脚本请求
+if [[ -z $current_Version ]]; then
+    echo "正在下载Vultr-Debian11脚本..."
+    download "Linux管理系统V" "已下载完成，即将进入系统！"   
+    
+#更新脚本请求
 else
     #强制更新
     if [ "$current_Version" == "force" ]; then
-    
-       wget --no-cache https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh -O /usr/local/bin/vinci
-       chmod +x /usr/local/bin/vinci
-       Version=$(cat /usr/local/bin/vinci | grep 'Version=' | cut -d'=' -f2 | head -1)
-       if [[ -z $Version ]]; then
-           echo "下载失败，请检查网络！"
-           sleep 5
-           exit 0
-       else
-           echo "管理系统V$Version强制更新已完成，即将重启管理系统！"
-           sleep 2
-           clear
-           exit 1
-       fi
+       echo "正在强制更新Vultr-Debian11脚本..."
+       download "Linux管理系统V" "已强制更新完成，即将重启管理系统！"
+
     #自动检查更新 
     else
         #检查最新版本号
+        echo "正在检查版本更新..."
         Version=$(curl -s https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh | grep 'Version=' | cut -d'=' -f2 | head -1)
+       
         #无需更新
         if [[ "${current_Version}" == "${Version}" ]]; then
            echo "当前已是最新版本(V$Version)，无需更新！"
-           sleep 2
+           sleep 1.5
            exit 0
+           
         #更新至最新版本
         else
-           echo "当前版本号为：V${current_Version}"
-           wget --no-cache https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh -O /usr/local/bin/vinci
-           chmod +x /usr/local/bin/vinci
-           echo "已更新至V$Version版本，即将重启管理系统！"
-           sleep 4
-           clear
-           exit 1
+           echo "当前版本号为：V${current_Version}，最新版本号为：V${Version}，即将更新！"
+           download "已更新至V" "版本，即将重启管理系统！"
         fi
     fi
 fi
-    
-    
-    
-    
-    
-    
-          vinci
-       #判断版本是否有错
-      if  [ ! $? == 0 ]; then
-           while true; do
-              echo "管理系统存在错误，正在尝试重新更新！...输入任意键退出"
-        read -t 59 -n 1 input
-        # 如果用户输入不为空，则退出更新
-        if [ ! -z $input ]; then
-            break
-        fi
-        clear
-        #强制更新
-        current_Version="force" bash <(curl -s -L https://raw.githubusercontent.com/vincilawyer/Bash/main/install-bash.sh)
-        vinci
-        #判断新版本是否有错
-        if  [ $? == 0 ]; then
-        break
-        fi
-     done
-  fi
-  exit
+  }
