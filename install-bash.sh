fce72def17e84284f63a411414884b5328fd56b5
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-04-10 10:12:12 +0800

    Update install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -18,7 +18,7 @@ else
         else
            wget --no-cache https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh -O /usr/local/bin/vinci
            chmod +x /usr/local/bin/vinci
-           echo "已更新至V$Version版本，即将进行管理系统！"
+           echo "已更新至V$Version版本，即将进入管理系统！"
            sleep 3
         fi
     fi
