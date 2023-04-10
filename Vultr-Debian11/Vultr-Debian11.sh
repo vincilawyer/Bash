fc0e2d1d0a27309662d613a0ae99d0fd407b034b
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-04-10 23:53:32 +0800

    Update Vultr-Debian11.sh

--- Vultr-Debian11/Vultr-Debian11.sh
+++ Vultr-Debian11/Vultr-Debian11.sh
@@ -20,7 +20,7 @@ option=0    #选项
 function countdown {
     local from=$1
     while [ $from -ge 0 ]; do
-        echo -ne "\r$froms \r"
+        echo -ne "\r${from}s \r"
         sleep 1
         ((from--))
     done
