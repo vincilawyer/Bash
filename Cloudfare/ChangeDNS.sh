93e56cd23c30cd1912dec5b27894fcf04c2276e9
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-04-24 12:03:17 +0800

    Update ChangeDNS.sh

--- Cloudfare/ChangeDNS.sh
+++ Cloudfare/ChangeDNS.sh
@@ -15,31 +15,42 @@ zone_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?nam
 if [ "$zone_identifier" == "null" ]; then
     echo "在您的Cloudflare账户中未找到该域名。"
 else
-    # 显示所有DNS解析记录
+   # 获取所有DNS解析记录、CDN代理状态和TTL
+   function get_all_dns_records {
     dns_records=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?type=A" \
          -H "X-Auth-Email: $email" \
          -H "X-Auth-Key: $api_key" \
-         -H "Content-Type: application/json" | jq -r '.result[] | [.name, .content] | @tsv')
-    echo "所有DNS解析记录："
-    echo "$dns_records"
-
+         -H "Content-Type: application/json" | jq -r '.result[] | [.name, .content, .proxied, .ttl] | @tsv')
+   }
+         
+  # 显示所有DNS解析记录、CDN代理状态和TTL
+   function all_dns_records {
+       echo "以下为当前$domain域名的所有DNS解析记录："
+       echo "$dns_records"
+       echo
+       echo
+   }
+    get_all_dns_records
+    all_dns_records
     # 询问用户要进行的操作
-    echo "请选择要进行的操作："
+    echo "操作选项："
     echo "1. 删除DNS记录"
     echo "2. 修改或增加DNS记录"
-    read choice
+    read -p "请选择要进行的操作：" choice
 
     if [ "$choice" == "1" ]; then
         # 删除DNS记录
-        echo "请输入要删除的DNS记录名称（例如 www）："
-        read record_name
+        clear
+        all_dns_records
+        read -p "请输入要删除的DNS记录名称（例如 www）：" record_name
 
         # 获取记录标识符
         record_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?type=A&name=$record_name.$domain" \
              -H "X-Auth-Email: $email" \
              -H "X-Auth-Key: $api_key" \
              -H "Content-Type: application/json" | jq -r '.result[0].id')
-
+        
+        clear
         # 如果记录标识符为空，则表示未找到该记录
         if [ "$record_identifier" == "null" ]; then
             echo "未找到该DNS记录。"
@@ -49,28 +60,27 @@ else
                  -H "X-Auth-Email: $email" \
                  -H "X-Auth-Key: $api_key" \
                  -H "Content-Type: application/json"
-            echo "已成功删除记录 $record_name.$domain"
+            echo "已成功删除DNS记录: $record_name.$domain"
+            get_all_dns_records
+            all_dns_records
         fi
     elif [ "$choice" == "2" ]; then
         # 修改或增加DNS记录
-        echo "请输入要修改或增加的DNS记录名称（例如 www）："
-        read record_name
+        clear
+        all_dns_records
+        read -p "请输入要修改或增加的DNS记录名称（例如 www）：" record_name
 
         # 验证IP地址
         ip_regex="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
         while true; do
-            echo "请输入IP地址："
-            read record_content
-
+            read -"请输入IP地址：" record_content
             if [[ $record_content =~ $ip_regex ]]; then
                 break
             else
                 echo "无效的IP地址，请重试。"
             fi
         done
-
-        echo "是否启用Cloudflare CDN代理？（Y/N）"
-        read enable_proxy
+        read -p "是否启用Cloudflare CDN代理？（Y/N）" enable_proxy
         if [[ $enable_proxy =~ ^[Yy]$ ]]; then
             proxy="true"
         else
@@ -82,7 +92,7 @@ else
                  -H "X-Auth-Email: $email" \
                  -H "X-Auth-Key: $api_key" \
                  -H "Content-Type: application/json" | jq -r '.result[0].id')
-
+            clear 
             # 如果记录标识符为空，则创建新记录
             if [ "$record_identifier" == "null" ]; then
                 curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records" \
@@ -91,6 +101,8 @@ else
                      -H "Content-Type: application/json" \
                      --data '{"type":"A","name":"'"$record_name"'","content":"'"$record_content"'","proxied":'"$proxy"'}'
                 echo "已成功添加记录 $record_name.$domain"
+                get_all_dns_records
+                all_dns_records
             else
                 # 如果记录标识符不为空，则更新现有记录
                 curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" \
@@ -99,14 +111,8 @@ else
                      -H "Content-Type: application/json" \
                      --data '{"type":"A","name":"'"$record_name"'","content":"'"$record_content"'","proxied":'"$proxy"'}'
                 echo "已成功更新记录 $record_name.$domain"
-                
-                # 显示所有DNS解析记录
-                dns_records=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?type=A" \
-                     -H "X-Auth-Email: $email" \
-                     -H "X-Auth-Key: $api_key" \
-                     -H "Content-Type: application/json" | jq -r '.result[] | [.name, .content] | @tsv')
-                echo "最新DNS解析记录："
-                echo "$dns_records"
+                get_all_dns_records
+                all_dns_records
            fi
     fi
 fi
