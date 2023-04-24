244521db3b32623461d85a76ff11dd2968ba4156
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-04-24 13:23:15 +0800

    Update Vultr-Debian11.sh

--- Vultr-Debian11/Vultr-Debian11.sh
+++ Vultr-Debian11/Vultr-Debian11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #版本号,不得为空
-Version=1.83
+Version=1.84
 
 #定义彩色字体
 RED='\033[0;31m'
@@ -183,7 +183,7 @@ function change_ssh_port {
     
     set "Port " " " $path_ssh "SSH端口" "0-65535，" "^([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$" true
     echo $option
-    if [ $option==1 ]; then
+    if option; then
           echo -e "${GREEN}正在将新端口添加进防火墙规则中。${NC}"
           ufw allow $text2/tcp
           echo -e "${GREEN}已正从防火墙规则中删除原SSH端口号：$text1${NC}"
