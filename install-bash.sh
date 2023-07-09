7278ada366f5fdf5a3428a71bb8d5e49c35bd8ba
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-07-09 19:42:02 +0800

    Update install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -1,119 +1,66 @@
-#!/bin/bash
+#!/bin/bash #################################################################################################################################################################################
+##############################################################################   vinci更新检查脚本源代码   ########################################################################################
+############################################################################################################################################################################################
 
-#定义等待时间
-Standby=59
-Ver=007
+####### 基本参数 ######
+Ver=008       #检查脚本版本号
+name="vinci"  #脚本名称
+#$current_Version 为当前版本号，由运行本脚本时传递该变量
+#$force           为是否强制更新，true为强制更新，由运行本脚本时传递该变量
+
+####### 路径 ######
 #Vultr-Debian11.sh文件网址
 link_Vultr_Debian11="https://raw.githubusercontent.com/vincilawyer/Bash/main/Vultr-Debian11/Vultr-Debian11.sh"
 #脚本保存路径
 download_path="/usr/local/bin"
-#脚本名称
-name="vinci"
 
-function countdown {
-    local from=$1
-    while [ $from -ge 0 ]; do
-        echo -ne "\r${from}s \r"
-        if $(read -s -t 1 -n 1); then
-        break
-        fi
-        ((from--))
-    done
-}
-
-function download {
-    wget --no-cache $link_Vultr_Debian11 -O $download_path/$name
-    chmod +x $download_path/$name
-    Version=$(sed -n '/^Version=/ {s/[^0-9.]*\([0-9.]*\).*/\1/; p; q}' $download_path/$name)
-    if [[ -z $Version ]]; then
-      echo "下载失败，请检查网络！"
-      countdown 3
-      exit 0
-    else
-      echo "$1$Version$2"
-      execute $Version
-    fi
-}
-
-function execute {
-    countdown 3
-    vinci
-    #如新版本存在错误
-    if  [ ! $? == 0 ]; then
-       #询问是否重新更新
-       if ! read -t $Standby -p "新版本存在错误，是否重新更新？(y/n):" input; then
-       input=y
-       fi
-       echo
-       if [ "$input"=="y" ]; then
-           while true; do
-              echo "新版本存在错误，正在尝试重新更新！...输入任意键退出"
-              #每隔59秒更新一次
-              for i in {1..$Standby}
-              do
-                 #监听输入同时每秒输出一个方块
-                 read -t 10 -n 1 -p "$(printf "\e[42m \e[0m")" input
-                 #如果有输入则退出监听
-                 if [ ! -z $input ]; then
-                    break
-                 fi  
-              done
-              
-              #如果有输入则退出更新
-              if [ ! -z $input ]; then
-                   break
-              fi  
-              #强制更新
-              wget --no-cache $link_Vultr_Debian11 -O $download_path/$name
-              chmod +x $download_path/$name
-              echo "重新更新完成，当前版本为V$1，正在尝试再次启动！"
-              sleep 3
-              vinci
-              #如新版本没有错误，则执行新版本
-              if  [ $? == 0 ]; then
-                 break
-              fi
-           done
-        else
-          echo "已放弃重新更新！"
-        fi
-     fi
-     exit 1
-}
-
+####### 主函数 ######
 function main {
 clear
-
+#检查最新版本号
+echo "正在检查最新版本($Ver)..."
+Version=$(curl -s $link_Vultr_Debian11 | sed -n '/^Version=/ {s/[^0-9.]*\([0-9.]*\).*/\1/; p; q}')
+        
 #下载脚本请求
 if [[ -z $current_Version ]]; then
     echo "正在下载Vultr-Debian11脚本..."
-    download "Linux管理系统V" "已下载完成，即将进入系统！"   
+    download "Linux管理系统V"$Version"版本已下载完成，即将进入系统！"   
     
-#更新脚本请求
-else
-    #强制更新
-    if [ "$current_Version" == "force" ]; then
-       echo "正在强制更新Vultr-Debian11脚本..."
-       download "Linux管理系统V" "已强制更新完成，即将重启管理系统！"
+#强制更新
+elif [ "$force" == "true" ]
+    echo "当前版本号为：V${current_Version}"
+    echo "最新版本号为：V${Version}，即将强制更新脚本..."
+    download "Linux管理系统V"$Version"版本已强制更新完成，即将重启管理系统！"
 
-    #自动检查更新 
-    else
-        #检查最新版本号
-        echo "正在检查版本更新($Ver)..."
-        Version=$(curl -s $link_Vultr_Debian11 | sed -n '/^Version=/ {s/[^0-9.]*\([0-9.]*\).*/\1/; p; q}')
-                                                                                                                     
-        #无需更新
-        if [[ "${current_Version}" == "${Version}" ]]; then
-           echo "当前已是最新版本(V$Version)，无需更新！"
-           exit 0
-           
-        #更新至最新版本
-        else
-           echo "当前版本号为：V${current_Version}，最新版本号为：V${Version}，即将更新！"
-           download "已更新至V" "版本，即将重启管理系统！"
-        fi
-    fi
+#已是最新
+elif [[ "${current_Version}" == "${Version}" ]]; then
+    echo "当前已是最新版本(V$Version)，无需更新！"
+    exit 0
+#正常更新
+else 
+    echo "当前版本号为：V${current_Version}"
+    echo "最新版本号为：V${Version}，即将更新脚本..."
+    download "Linux管理系统V"$Version"版本已更新完成，即将进入系统！"
 fi
   }
+  
+####### 下载vinci脚本 ###### 
+function download {
+    if wget --no-cache "$link_Vultr_Debian11" -O $download_path/"$name" ; then
+        chmod +x $download_path/$name
+        echo "$1"
+        vinci
+    else
+        echo "下载失败，请检查网络！"
+        sleep 3
+        exit
+    fi    
+} 
+
+
+
+
+
+
 
 main
