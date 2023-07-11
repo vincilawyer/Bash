5929af1fab0ea14f8d394819f3d10492be9f9a13
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-07-11 23:40:19 +0800

    Update install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -58,8 +58,9 @@ function main {
              done
          fi
     else
-        echo -n "vinci脚本下载失败，请检查网络！"
-        exit 3                                                     #未更新脚本的返回值       
+        echo -n "vinci脚本下载失败，请检查网络！即将返回..."
+        countdown 5
+        exit 2                                                     #未更新脚本的返回值       
     fi
 }
 
@@ -79,10 +80,10 @@ function main {
             echo -e "${RED}########################################################################################${NC}"
             read -t 1 -n 1 input  #读取输入，在循环中一次1秒
             if [ -n "$input" ] ; then
-                if bar 15 "已取消继续更新，即将尝试回滚至旧版本" "开始回滚" true "已取消回滚！"; then exit 4; fi 
+                if bar 15 "已取消继续更新，即将尝试回滚至旧版本" "开始回滚" true "已取消回滚！即将返回..."; then exit; fi 
                 cp -f "$file_path"_backup "$file_path" 
-                echo "已回滚至旧版本！"
-                exit 4           #脚本运行错误，取消更新的返回值
+                echo "已回滚至旧版本！即将返回..."
+                exit 2           #脚本运行错误，取消更新的返回值
             fi
             if ! ((t % 50 == 0)); then continue; fi  #每隔50s检查一次更新情况
             code="$(curl -s "$link_Vultr_Debian11")" && Version=$( echo "$code" | sed -n '/^Version=/ {s/[^0-9.]*\([0-9.]*\).*/\1/; p; q}')
