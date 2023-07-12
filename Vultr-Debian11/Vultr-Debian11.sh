86aa44758c2c4b11c44d56b8740d1ced5692199a
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-07-12 16:43:45 +0800

    更新 Vultr-Debian11.sh

--- Vultr-Debian11/Vultr-Debian11.sh
+++ Vultr-Debian11/Vultr-Debian11.sh
@@ -1387,8 +1387,8 @@ docker run -d -p 3000:3000 \
    -e OPENAI_API_KEY="$Chatgpt_api_key" \
    -e CODE="$Gpt_code" \
    -e BASE_URL="$BASE_URL" \
-   --net=host \
-   -e PROXY_URL="$PROXY_URL" \
+   #--net=host \
+   #-e PROXY_URL="$PROXY_URL" \
    yidadaa/chatgpt-next-web
 }
 ###### 消息推送 ######
