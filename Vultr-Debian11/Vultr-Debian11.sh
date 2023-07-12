ddb0c35957c564c2abe900998d1ee9d17f5fcccc
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-07-12 16:26:09 +0800

    更新 Vultr-Debian11.sh

--- Vultr-Debian11/Vultr-Debian11.sh
+++ Vultr-Debian11/Vultr-Debian11.sh
@@ -23,7 +23,7 @@
 
 
 ####### 版本更新相关参数 ######
-Version=3.07  #版本号 
+Version=3.08  #版本号 
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"      #获取当前脚本的目录路径
 script_name="$(basename "${BASH_SOURCE[0]}")"                                     #获取当前脚本的名称
 file_path="$script_path/$script_name"                                             #获取当前脚本的文件路径
