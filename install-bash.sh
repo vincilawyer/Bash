650e26bf45385517c5aab1d74f32bcbc7030deb1
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-07-12 19:51:18 +0800

    更新 install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -33,8 +33,7 @@ num='$(n="$(cat "$file_path")" &&  echo "${#n}")'                               
 
 ####### 主函数 ######
 function main {
-    if [ "$wrong" == "1" ]; then warning; fi 
-    clear
+    (( wrong==1)) ｜｜ clear
     if code="$(curl -s "$link_Vultr_Debian11")"; then  
          if [ -e "$file_path" ] && [ "$code" == "$(cat "$file_path")" ]; then
               [ -z "$cur_Version" ] && cur_Version=$(sed -n '/^Version=/ {s/[^0-9.]*\([0-9.]*\).*/\1/; p; q}' "$file_path")
