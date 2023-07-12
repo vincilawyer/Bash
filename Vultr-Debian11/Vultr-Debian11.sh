8a79a6f1e40d3fd74afc9da815f1ec97ac77e5e6
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-07-12 14:30:59 +0800

    更新 Vultr-Debian11.sh

--- Vultr-Debian11/Vultr-Debian11.sh
+++ Vultr-Debian11/Vultr-Debian11.sh
@@ -402,7 +402,8 @@ function update_dat {
 }
 ######   写入用户数据  ######
 function write_dat {
-for ((i = 0; i <= 1; i++)); do; dat_text="
+    for ((i = 0; i <= 1; i++)); do
+    dat_text="
 
 # 该文件为vinci用户配置文本
 # * 表示不可在脚本中修改的常量,变量值需要用双引号包围, #@ 用于分隔变量名称、备注、匹配正则表达式。
@@ -419,7 +420,7 @@ $(pz "Gpt_code" i)                                #@授权码
 $(pz "BASE_URL" i)                                #@OpenAI接口代理URL
 $(pz "PROXY_URL" i)                               #@Chatgpt本地代理地址
 
-";(( i==0 )) && dat_num=${#dat_text};done;echo "$dat_text" > "$dat_path";}
+    ";(( i==0 )) && dat_num=${#dat_text};done;echo "$dat_text" > "$dat_path";}
 
 ###### 为用户配置模板写入数据 ######
 function pz { echo "$1=\"$((($2==1)) && eval echo \$"$1")\""; }
