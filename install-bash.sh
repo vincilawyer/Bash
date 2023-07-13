5b41acce7750d09dc439cbecbe2f890ebe4d5cee
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-07-13 11:31:56 +0800

    Update install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -95,7 +95,7 @@ function main {
             echo -e "${RED}#####$b#####${NC}"
             echo -e "${RED}###################################################################################${NC}"
             read -t 1 -n 1 input  #读取输入，在循环中一次1秒
-            if [ -n "$input" ] ; then
+            if [ -n "$input" ] || [ $？ -eq 142 ] ; then
                 if bar 15 "已取消继续更新，即将尝试回滚至旧版本" "开始回滚" true "已取消回滚！即将返回..."; then exit 1; fi 
                 cp -f "$file_path"_backup "$file_path" 
                 echo
@@ -122,10 +122,13 @@ function bar() {
        echo -e "\033[1F\033[1G$block"$2"···"
        printf "输入任意键可退出...%02ds" $time
        read -t 1 -n 1 input
-           if [ -n "$input" ] && [[ $4 == "true" ]]; then
+           if [ -n "$input" ] || [ $？ -eq 142 ] && [[ $4 == "true" ]]; then
                echo "$5"
                return 0 
            fi  
+if [ $？ -eq 142 ]; then
+
+
     done       
     echo
     printf "\033[1A\033[K%s\n" "$3"
