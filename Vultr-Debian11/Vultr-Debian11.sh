aab205c8c3954ffa4f2bea3a34616a091431ad2a
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-07-13 23:44:44 +0800

    Update Vultr-Debian11.sh

--- Vultr-Debian11/Vultr-Debian11.sh
+++ Vultr-Debian11/Vultr-Debian11.sh
@@ -747,11 +747,11 @@ function settext {
                      echo -e "${BLUE}已将"$mean"参数设为注释行${NC}"
                      return 0
                  else
-                                      echo 2
+                       echo 2
                      cat "$dat_path"
                      replace  "$start_string" "$end_string" "$location_string" "$n" "$exact_match" "$module" "$comment" "$is_file" "$input" "$new_text"
                      echo -e "${BLUE}"$mean"已修改为"$new_text"${NC}"
-                                          echo 3
+                     echo 3
                      cat "$dat_path"
                      return 0
                  fi
