55483af1cf01c95f71fb935c1d288130d51a7d75
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-07-13 23:02:29 +0800

    Update Vultr-Debian11.sh

--- Vultr-Debian11/Vultr-Debian11.sh
+++ Vultr-Debian11/Vultr-Debian11.sh
@@ -729,7 +729,7 @@ function settext {
      old_text1=$(search "$start_string" "$end_string" "$location_string" "$n" "$exact_match" "$module" "true" "$is_file" "$input")
      old_text=${old_text1// (注释行)/}
      echo
-     echo -e "${BLUE}【"$mean"设置】${NC}${GREEN}当前的"$mean"为$([ -n "$old_text1" ] || echo "空" && echo "为：$old_text1")${NC}"
+     echo -e "${BLUE}【"$mean"设置】${NC}${GREEN}当前的"$mean"为$([ -z "$old_text1" ] && echo "空" || echo "为：$old_text1")${NC}"
      while true; do
          #-r选项告诉read命令不要对反斜杠进行转义，避免误解用户输入。-e选项启用反向搜索功能，这样用户在输入时可以通过向左箭头键或Ctrl + B键来移动光标并修改输入。
          echo -ne "${GREEN}请设置新的$mean（$( [ -n "$mark" ] && echo "$mark,")输入为空则跳过$( [[ $coment == "true" ]] && echo "，输入#则设为注释行")）：${NC}"
