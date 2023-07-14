5888f20422faa78dd9448b8993a3000ffff17c5f
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-07-14 23:21:58 +0800

    Update Vultr-Debian11.sh

--- Vultr-Debian11/Vultr-Debian11.sh
+++ Vultr-Debian11/Vultr-Debian11.sh
@@ -700,7 +700,7 @@ function settext {
      echo -e "${BLUE}【"$mean"设置】${NC}${GREEN}当前的"$mean"为$([ -z "$old_text1" ] && echo "空" || echo "：$old_text1")${NC}"
      while true; do
          #-r选项告诉read命令不要对反斜杠进行转义，避免误解用户输入。-e选项启用反向搜索功能，这样用户在输入时可以通过向左箭头键或Ctrl + B键来移动光标并修改输入。
-         echo -ne "${GREEN}请设置新的$mean（$( [ -n "$mark" ] && echo "$mark,")输入为空则跳过$( [[ $coment == "true" ]] && echo "，输入#则设为注释行")）：${NC}"
+         echo -ne "${GREEN}请设置新的$mean（$( [ -n "$mark" ] && echo "$mark,")输入为空则跳过$( [[ $coment == "true" ]] && echo "，输入#则设为注释行" || echo "，输入#则设为空值" )）：${NC}"
          inp true ${@:12} $( [ -n "${13}" ] && echo "#" )  
          if [[ -z "$new_text" ]]; then
              echo -e "${GREEN}已跳过$mean设置${NC}"
@@ -713,8 +713,9 @@ function settext {
                      return 0
                  else
                      [[ "$new_text" == "#" ]] && new_text=""
+                     [[ "$new_text" == '"#"' ]] && new_text="#"
                      replace  "$start_string" "$end_string" "$location_string" "$n" "$exact_match" "$module" "$comment" "$is_file" "$input" "$new_text"
-                     echo -e "${BLUE}"$mean"已修改为"$new_text"${NC}"
+                     echo -e "${BLUE}"$mean"已修改为"$([ -z "$new_text" ] && echo "空" || echo "：$new_text")"${NC}"
                      return 0
                  fi
             else                           #如果在文本模式下
