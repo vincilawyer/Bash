212b0811dbfd14b195965265ba13dbc3c1dbd6c0
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-08-01 16:19:33 +0800

    Update install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -47,7 +47,7 @@ function main {
     (( wrong==1 )) || clear
     while true; do
          #如果未获取到新版本文件
-        if ! code="$(curl -s "$link_Vultr_Debian11")"; then  
+        if ! code="$(curl -s "$link_vinci")"; then  
             echo -n "vinci脚本下载失败，请检查网络！即将返回..."
             countdown 10
             exit 0
