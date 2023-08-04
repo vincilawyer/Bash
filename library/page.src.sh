8188b12990a007c554c14cc4da28c03ae8e047f5
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-08-04 19:16:11 +0800

    Update page.src.sh

--- library/page.src.sh
+++ library/page.src.sh
@@ -35,9 +35,9 @@ function page {
     echo
     echo -n "  请按序号选择操作: "
     inp false 1 '"[[ "$new_text" =~ ^[0-9]+$ ]] && (( $new_text >= 0 && $new_text <= '$((menunum))' ))"'
-    [ "$new_text" == "0" ] && quit              #如果选择零则退出
+    [[ "$new_text" == "0" ]] && quit              #如果选择零则退出
     clear 
     eval ${cmd[$((new_text-1))]}  || ( echo "指令执行可能失败，请检查！"; waitcon="true" )
-    [ "$waitcon" == "true" ] && wait
+    [[ "$waitcon" == "true" ]] && wait
 done
 }
