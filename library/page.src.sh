c687072082c9c713040d43f3fd40319ec7d29763
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-08-05 01:12:30 +0800

    Update page.src.sh

--- library/page.src.sh
+++ library/page.src.sh
@@ -17,9 +17,9 @@ function page {
     menu=()
     cmd=()
     n=1
-    
+
     #分离菜单和指令
-    for (( i=-1; i<${#array[@]}; i++ )); do
+    for (( i=0; i<${#array[@]}; i++ )); do
         if (( i % 2 == 0 )) ; then
             menu+=("${array[$i]}")
             echo "  [$n]$(((n<10)) && echo " ") ${array[$i]}" 
@@ -29,7 +29,11 @@ function page {
         fi
     done
        echo "  [0]  退出"
-       
+
+       echo ${array[1]}
+       echo ${array[2]}
+       echo ${array[3]}
+       echo ${array[4]}
     #获取菜单数量
     menunum=${#menu[@]} 
     echo
