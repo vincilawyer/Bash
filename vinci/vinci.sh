093f210850718d5cee25fc56483ad99277b7f51a
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-08-04 14:52:44 +0800

    Update vinci.sh

--- vinci/vinci.sh
+++ vinci/vinci.sh
@@ -52,6 +52,7 @@ local funcname2="$4"
    else
         clear
    fi            
+   echo -e "${GREED}已退出vinci脚本！${NC}"
    exit
 }
 
@@ -63,7 +64,7 @@ function handle_error() {
 
 #######   当脚本退出   ####### 
 function normal_exit() { 
-   echo -e "${GREED}已退出vinci脚本！${NC}"
+:
 }
 
 #######   脚本退出前执行  #######   
