4c310cb384f7f2ccd066d9fd133ccab5ba0455c6
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-08-04 18:47:16 +0800

    Update vinci.sh

--- vinci/vinci.sh
+++ vinci/vinci.sh
@@ -1,6 +1,4 @@
 #!/bin/zsh 
-setopt interactivecomments
-export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;export ALL_PROXY=socks5://127.0.0.1:1086
 #程序组成结构:1、启动程序（即本程序）,用于下载、启动更新update程序和主程序（该两程序及本程序容错率为0）。
 #2、update程序，用于更新、加载配置文件。
 #3、主程序，诠释程序作用。
@@ -8,39 +6,50 @@ export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087
 Version=2.00  #版本号 
 
 ####### 定义本脚本名称、应用数据路径 ######
-def_name="vinci"                                                                         #启动程序脚本默认名称
-data_name="$HOME/myfile"                                                                 #应用数据文件夹位置路径                   
-data_path="$data_name/${def_name}_src"                                                   #应用数据文件夹位置名
-mkdir "$data_name"  >/dev/null 2>&1                                                      #创建文件夹 
-mkdir "$data_path"  >/dev/null 2>&1                                                      #应用资源文件夹
+#启动程序脚本默认名称
+def_name="vinci" 
+#应用数据文件夹位置路径 
+data_name="$HOME/myfile" 
+#应用数据文件夹位置名
+data_path="$data_name/${def_name}_src"                                                   
+#创建文件夹 
+mkdir "$data_name"  >/dev/null 2>&1                                                      
+#应用资源文件夹
+mkdir "$data_path"  >/dev/null 2>&1                                                      
 
 #### 配置文件、程序网址、路径 ####
 #仓库
 link_repositories="https://raw.githubusercontent.com/vincilawyer/Bash/main/"            
 #update.src.sh
-link_update="${link_repositories}library/update.src.sh"                                  #更新检查程序网址
-path_update="$data_path/update.src.sh"                                                   #更新检查程序路径
+link_update="${link_repositories}library/update.src.sh"                                  
+path_update="$data_path/update.src.sh"                                                
 #main.src.sh
-link_main="${link_repositories}vinci/main.src.sh"                                      #主程序网址
-path_main="$data_path/main.src.sh"                                                     #主程序路径
-#
-link_def="${link_repositories}vinci/vinci.sh"                                                  #启动程序下载网址
+link_main="${link_repositories}vinci/main.src.sh"                                    
+path_main="$data_path/main.src.sh"                                                    
+#启动程序下载网址
+link_def="${link_repositories}vinci/vinci.sh"                                                  
 
 ####### Android系统启动程序网址、路径 ######
       if uname -a | grep -q 'Android'; then echo '检测系统为Android，正在配置中...' 
-path_def="/data/data/com.termux/files/usr/bin/$def_name"                                           #启动程序目录路径
+path_def="/data/data/com.termux/files/usr/bin/$def_name"                                           
         
 ####### Debian系统启动程序网址、路径 ######
       elif uname -a | grep -q 'Debian'; then echo '检测系统为Debian，正在配置中...'
-path_def="/usr/local/bin/$def_name"                                                                #启动程序目录路径
+path_def="/usr/local/bin/$def_name"                                                               
 
 ####### Mac系统启动程序网址、路径 ######
       elif uname -a | grep -q 'Darwin'; then echo '检测系统为Mac，正在配置中...'
-path_def="/Users/Vinci/Desktop/$def_name"                                                                #启动程序目录路径
-
+path_def="/Users/Vinci/Desktop/$def_name"
+      #切换运行环境
+      [[ $(ps -p $$ -o comm=) == *"bash"* ]] && exec "/bin/zsh" "$0" && exit
+      #允许注释与代码同行
+      setopt interactivecomments
+      #打开网络代理
+      export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;export ALL_PROXY=socks5://127.0.0.1:1086
+      
 ###### 其他系统启动程序网址、路径 ######
       else echo '未知系统，正在配置默认版本中...'
-path_def="/usr/local/bin/$def_name"                                                                #启动程序目录路径
+path_def="/usr/local/bin/$def_name"                                                             
       fi    
 
 ######  退出函数 ######      
