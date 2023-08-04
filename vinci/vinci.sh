d249a3f878f09a5bb85fae1b829c1d52f5f40627
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-08-04 11:01:33 +0800

    Update vinci.sh

--- vinci/vinci.sh
+++ vinci/vinci.sh
@@ -13,40 +13,31 @@ data_path="$data_name/${def_name}_src"                                          
 mkdir "$data_name"  >/dev/null 2>&1                                                      #创建文件夹 
 mkdir "$data_path"  >/dev/null 2>&1                                                      #应用资源文件夹
 
-####### Android系统启动程序网址、路径 ######
-      if uname -a | grep -q 'Android'; then echo '检测系统为Android，正在配置中...' 
-path_def="/data/data/com.termux/files/usr/bin/$def_name"                                           #启动程序目录路径
-link_def="${link_repositories}Android/Android.sh"                                                  #启动程序下载网址
-         
-####### Debian系统启动程序网址、路径 ######
-      elif uname -a | grep -q 'Debian'; then echo '检测系统为Debian，正在配置中...'
-path_def="/usr/local/bin/$def_name"                                                                #启动程序目录路径
-link_def="${link_repositories}Vultr-Debian11/Vultr-Debian11.sh"  
-
-###### 其他系统启动程序网址、路径 ######
-      else echo '未知系统，正在配置默认版本中...'
-path_def="/usr/local/bin/$def_name"                                                                #启动程序目录路径
-link_def="${link_repositories}Vultr-Debian11.sh" 
-      fi    
-      
 #### 配置文件、程序网址、路径 ####
+#仓库
+link_repositories="https://raw.githubusercontent.com/vincilawyer/Bash/main/"            
 #update.src.sh
-link_repositories="https://raw.githubusercontent.com/vincilawyer/Bash/main/"             #仓库网址
 link_update="${link_repositories}library/update.src.sh"                                  #更新检查程序网址
 path_update="$data_path/update.src.sh"                                                   #更新检查程序路径
 #main.src.sh
 link_main="${link_repositories}vinci/main.src.sh"                                      #主程序网址
 path_main="$data_path/main.src.sh"                                                     #主程序路径
+#
+link_def="${link_repositories}vinci/vinci.sh"                                                  #启动程序下载网址
 
-#######   等待函数   #######   
-function wait {
-   if [[ -z "$1" ]]; then
-    echo "请按下任意键继续"
-   else
-    echo $1
-   fi
-   read -n 1 -s input
-}
+####### Android系统启动程序网址、路径 ######
+      if uname -a | grep -q 'Android'; then echo '检测系统为Android，正在配置中...' 
+path_def="/data/data/com.termux/files/usr/bin/$def_name"                                           #启动程序目录路径
+        
+####### Debian系统启动程序网址、路径 ######
+      elif uname -a | grep -q 'Debian'; then echo '检测系统为Debian，正在配置中...'
+path_def="/usr/local/bin/$def_name"                                                                #启动程序目录路径
+
+###### 其他系统启动程序网址、路径 ######
+      else echo '未知系统，正在配置默认版本中...'
+path_def="/usr/local/bin/$def_name"                                                                #启动程序目录路径
+      fi    
+      
 
 #下载更新检查程序
 if ! curl -H 'Cache-Control: no-cache' -L "$link_update" -o "$path_update" >/dev/null; then echo "更新检查程序下载失败，请检查网络！"; wait; fi
