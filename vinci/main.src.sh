58bf2fa9efd21e2deb0af843c95eba1522c0e6f2
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-08-04 15:07:33 +0800

    Update main.src.sh

--- vinci/main.src.sh
+++ vinci/main.src.sh
@@ -177,23 +177,23 @@ if uname -a | grep -q 'Debian'; then
     fi
     
     #载入通用模块
-    update_load "$path_arg" "$link_arg" "基本参数模块" 1 true
-    update_load "$path_text_processing" "$link_text_processing" "文本处理模块" 1 true
-    update_load "$path_page" "$link_page" "页面显示模块" 1 true
-    update_load "$path_config" "$link_config" "用户配置模块" 1 true
-    update_load "$path_toolbox_universal" "$link_toolbox_universal" "通用工具模块" 1 true
+    update_load "$path_arg" "$link_arg" "基本参数模块" 1
+    update_load "$path_text_processing" "$link_text_processing" "文本处理模块" 1
+    update_load "$path_page" "$link_page" "页面显示模块" 1
+    update_load "$path_config" "$link_config" "用户配置模块" 1
+    update_load "$path_toolbox_universal" "$link_toolbox_universal" "通用工具模块" 1
     #载入专属模块
-    update_load "$path_toolbox_linux" "$link_toolbox_linux" "linux工具模块" 1 false
-    update_load "$path_appmanage" "$link_appmanage" "程序管理模块" 1 false
-    update_load "$path_docker" "$link_docker" "docker" 1 false
-    update_load "$path_nginx" "$link_nginx" "nginx" 1 false
-    update_load "$path_xui" "$link_xui" "xui" 1 false
-    update_load "$path_cf" "$link_cf" "cloudflare" 1 false
-    update_load "$path_tor" "$link_tor" "tor" 1 false
-    update_load "$path_frp" "$link_frp" "frp" 1 false
-    update_load "$path_chatgpt" "$link_chatgpt" "chatgpt" 1 false
-    update_load "$path_alist_linux" "$link_alist_linux" "alist_linux" 1 false
-    update_load "$path_rclone_linux" "$link_rclone_linux" "rclone_linux" 1 false 
+    update_load "$path_toolbox_linux" "$link_toolbox_linux" "linux工具模块" 1
+    update_load "$path_appmanage" "$link_appmanage" "程序管理模块" 1
+    update_load "$path_docker" "$link_docker" "docker" 1
+    update_load "$path_nginx" "$link_nginx" "nginx" 1
+    update_load "$path_xui" "$link_xui" "xui" 1
+    update_load "$path_cf" "$link_cf" "cloudflare" 1
+    update_load "$path_tor" "$link_tor" "tor" 1
+    update_load "$path_frp" "$link_frp" "frp" 1
+    update_load "$path_chatgpt" "$link_chatgpt" "chatgpt" 1
+    update_load "$path_alist_linux" "$link_alist_linux" "alist_linux" 1
+    update_load "$path_rclone_linux" "$link_rclone_linux" "rclone_linux" 1 
 
 #### 主菜单 ####
 main_menu=(
@@ -228,14 +228,14 @@ elif uname -a | grep -q 'Android'; then
     echo '检测系统为Android，正在配置中...'
     
     #载入通用模块
-    update_load "$path_arg" "$link_arg" "基本参数模块" 1 true
-    update_load "$path_text_processing" "$link_text_processing" "文本处理模块" 1 true
-    update_load "$path_page" "$link_page" "页面显示模块" 1 true
-    update_load "$path_config" "$link_config" "用户配置模块" 1 true
-    update_load "$path_toolbox_universal" "$link_toolbox_universal" "通用工具模块" 1 true
+    update_load "$path_arg" "$link_arg" "基本参数模块" 1
+    update_load "$path_text_processing" "$link_text_processing" "文本处理模块" 1
+    update_load "$path_page" "$link_page" "页面显示模块" 1
+    update_load "$path_config" "$link_config" "用户配置模块" 1
+    update_load "$path_toolbox_universal" "$link_toolbox_universal" "通用工具模块" 1
     ######载入专属模块 ######
-    update_load "$path_alist_andriod" "$link_alist_andriod" "alist_andriod" 1 false
-    update_load "$path_rclone_andriod" "$link_rclone_andriod" "rclone_andriod" 1 false
+    update_load "$path_alist_andriod" "$link_alist_andriod" "alist_andriod" 1
+    update_load "$path_rclone_andriod" "$link_rclone_andriod" "rclone_andriod" 1
 
 main_menu=(
     "系统设置"              'page true " 系 统 设 置 " "${system_menu[@]}"' 
