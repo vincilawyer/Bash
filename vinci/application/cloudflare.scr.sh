ef7c03d7df6f4bfa2f5fe81d02e472578535fa55
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-08-06 03:18:12 +0800

    更新 cloudflare.scr.sh

--- vinci/application/cloudflare.scr.sh
+++ vinci/application/cloudflare.scr.sh
@@ -259,7 +259,7 @@ function start_speedtest {
         set_dat "SPEEDTEST_URL"
     fi
     echo "开始测速，请稍等..."
-    $path_CFST_file/CloudflareST -n 400 -t 6 -tl 200 -tlr 0.2 -sl 30 -allip -url https://www.dvbh3bhvzvavdsne7h2cds.world/download/speedtest.bin
+    $path_CFST_file/CloudflareST -n 400 -t 6 -tl 180 -tlr 0 -sl 30 -url https://www.dvbh3bhvzvavdsne7h2cds.world/download/speedtest.bin
     notifier "IP优选测速结果如下：\n$(cat result.csv)"
 }
 
