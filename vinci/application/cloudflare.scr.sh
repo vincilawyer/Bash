39c6a9159b94f501009305ff7cda6e0a371b617b
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-08-06 01:12:17 +0800

    Update cloudflare.scr.sh

--- vinci/application/cloudflare.scr.sh
+++ vinci/application/cloudflare.scr.sh
@@ -229,7 +229,25 @@ function install_CFST {
     tar -zxf "$path_CFST_file/CFST.tar.gz" -C "$path_CFST_file"
     chmod +x "$path_CFST_file/CloudflareST"
     rm "$path_CFST_file/CFST.tar.gz"
+cat > "$path_CFST_file/ip.text" <<EOF
+103.21.244.0/22
+103.22.200.0/22
+103.31.4.0/22
+104.16.0.0/13
+104.24.0.0/14
+108.162.192.0/18
+131.0.72.0/22
+141.101.64.0/18
+162.158.0.0/15
+172.64.0.0/13
+173.245.48.0/20
+188.114.96.0/20
+190.93.240.0/20
+197.234.240.0/22
+198.41.128.0/17 
+EOF
     echo "安装完成！"
+    
 }
 function Creat_cfspeedtest {
     speedtest_name="speedtest.bin"
