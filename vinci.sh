7502f32f35c4837b70b925413d33a4ee6f6c569a
vincilawyer <130241460+vincilawyer@users.noreply.github.com>
2023-11-24 20:00:00 +0800

    Update vinci.sh

--- vinci.sh
+++ vinci.sh
@@ -81,10 +81,10 @@ elif uname -a | grep -q 'Android'; then
     # 安装依赖件 ncurses-utils，以支持tput工具
     which "tput" >/dev/null || (echo "正在更新系统软件"; rm -rf $PREFIX/etc/apt/sources.list.d/game.list; \
     rm -rf $PREFIX/etc/apt/sources.list.d/science.list; echo "deb https://packages.termux.org/apt/termux-main stable main" > $PREFIX/etc/apt/sources.list; \
-    apt update; apt upgrade -y; apt install ncurses-utils -y; echo "系统软件已更新完成并已安装依赖件ncurses-utils！")
+    apt update; apt upgrade -y; apt install which ncurses-utils jq -y; echo "系统软件已更新完成并已安装依赖件Which、ncurses-utils、JQ！")
     
     # 安装依赖件jq
-    which "jq" >/dev/null || (echo "正在安装依赖软件JQ..."; apt install jq -y; echo "依赖件JQ已安装完成！")
+    #which "jq" >/dev/null || (echo "正在安装依赖软件JQ..."; apt install jq -y; echo "依赖件JQ已安装完成！")
                                                                 
 ####### Mac系统启动程序网址、路径 ######
 elif uname -a | grep -q 'Darwin'; then 
